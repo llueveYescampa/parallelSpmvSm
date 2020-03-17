dbfc9501225384be77e6701d6f0c3840a71ef3d9
Edgar F. Black <edgarfblack@gmail.com>
2020-03-17 11:00:05 -0500

    new runAll script

--- runAll.sh
+++ runAll.sh
@@ -1,39 +1,119 @@
 #!/bin/bash
-if [ "$#" -ne 1 ]
-then
-  echo "Usage: $0 matrixName"
+if [ "$#" -lt 1 ]; then
+  echo "Usage: $0 matrixName [numOfProcesses]"
   exit 1
 fi
+n=4
+if [ "$#" -eq 2 ]; then
+    n=$2
+fi
+echo "number of threads or processes: $n"
 matrix=$1
 
+nloops=5
+
+tempFilename=$(hostname)'_anyTempFileNameWillWork.txt'
+mkdir -p ../plots/$(hostname)'_'$n/Gnu/$matrix/
+mkdir -p ../plots/$(hostname)'_'$n/Intel/$matrix/
+mkdir -p ../plots/$(hostname)'_'$n/Pgi/$matrix/
+#outputFilename=$1.txt
+
 cd buildGnu
-make clean
-make -j
 
-../runTest.sh parallelSpmvSm    $matrix gnu
-
-cd ../
-
-source setIcc intel64
-source setImpi
-
-cd buildIntel
-make clean
-make -j
-
-../runTest.sh parallelSpmvSm    $matrix intel
+rm -f $tempFilename
+for j in  `seq 1 $nloops`; do
+    run raptor_Normal $matrix $n | grep by >>  $tempFilename
+done
+cat $tempFilename  | grep process | awk 'BEGIN{}   { printf("%f  %f\n", $7,$10)}  END{}' |  sort  -k1,1n -k2,2n |   head -1  > ../../plots/$(hostname)'_'$n/Gnu/$matrix/raptor_Normal.txt
 
 
-cd ../
+rm -f $tempFilename
+for j in  `seq 1 $nloops`; do
+    run raptor_TAP $matrix $n | grep by >>  $tempFilename
+done
+cat $tempFilename  | grep process | awk 'BEGIN{}   { printf("%f  %f\n", $7,$10)}  END{}' |  sort  -k1,1n -k2,2n |   head -1  > ../../plots/$(hostname)'_'$n/Gnu/$matrix/raptor_TAP.txt
 
-source setPgi 19.x
-source setPgiMpi 19.x
 
-cd buildPgi
-make clean
-make -j
+rm -f $tempFilename
+for j in  `seq 1 $nloops`; do
+    run parallelSpmv $matrix $n | grep by >>  $tempFilename
+done
+cat $tempFilename  | grep process | awk 'BEGIN{}   { printf("%f  %f\n", $7,$10)}  END{}' |  sort  -k1,1n -k2,2n |   head -1  > ../../plots/$(hostname)'_'$n/Gnu/$matrix/parallelSpmv.txt
 
-../runTest.sh parallelSpmvSm    $matrix pgi
+rm -f $tempFilename
+for j in  `seq 1 $nloops`; do
+    run parallelSpmvSm $matrix $n | grep by >>  $tempFilename
+done
+cat $tempFilename  | grep process | awk 'BEGIN{}   { printf("%f  %f\n", $7,$10)}  END{}' |  sort  -k1,1n -k2,2n |   head -1  > ../../plots/$(hostname)'_'$n/Gnu/$matrix/parallelSpmvSm.txt
 
-cd ../
+rm $tempFilename
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+cd ../buildIntel
+
+rm -f $tempFilename
+for j in  `seq 1 $nloops`; do
+    run raptor_Normal $matrix $n | grep by >>  $tempFilename
+done
+cat $tempFilename  | grep process | awk 'BEGIN{}   { printf("%f  %f\n", $7,$10)}  END{}' |  sort  -k1,1n -k2,2n |   head -1  > ../../plots/$(hostname)'_'$n/Intel/$matrix/raptor_Normal.txt
+
+
+rm -f $tempFilename
+for j in  `seq 1 $nloops`; do
+    run raptor_TAP $matrix $n | grep by >>  $tempFilename
+done
+cat $tempFilename  | grep process | awk 'BEGIN{}   { printf("%f  %f\n", $7,$10)}  END{}' |  sort  -k1,1n -k2,2n |   head -1  > ../../plots/$(hostname)'_'$n/Intel/$matrix/raptor_TAP.txt
+
+
+rm -f $tempFilename
+for j in  `seq 1 $nloops`; do
+    run parallelSpmv $matrix $n | grep by >>  $tempFilename
+done
+cat $tempFilename  | grep process | awk 'BEGIN{}   { printf("%f  %f\n", $7,$10)}  END{}' |  sort  -k1,1n -k2,2n |   head -1  > ../../plots/$(hostname)'_'$n/Intel/$matrix/parallelSpmv.txt
+
+rm -f $tempFilename
+for j in  `seq 1 $nloops`; do
+    run parallelSpmvSm $matrix $n | grep by >>  $tempFilename
+done
+cat $tempFilename  | grep process | awk 'BEGIN{}   { printf("%f  %f\n", $7,$10)}  END{}' |  sort  -k1,1n -k2,2n |   head -1  > ../../plots/$(hostname)'_'$n/Intel/$matrix/parallelSpmvSm.txt
+
+rm $tempFilename
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+cd ../buildPgi
+
+rm -f $tempFilename
+for j in  `seq 1 $nloops`; do
+    run raptor_Normal $matrix $n | grep by >>  $tempFilename
+done
+cat $tempFilename  | grep process | awk 'BEGIN{}   { printf("%f  %f\n", $7,$10)}  END{}' |  sort  -k1,1n -k2,2n |   head -1  > ../../plots/$(hostname)'_'$n/Pgi/$matrix/raptor_Normal.txt
+
+
+rm -f $tempFilename
+for j in  `seq 1 $nloops`; do
+    run raptor_TAP $matrix $n | grep by >>  $tempFilename
+done
+cat $tempFilename  | grep process | awk 'BEGIN{}   { printf("%f  %f\n", $7,$10)}  END{}' |  sort  -k1,1n -k2,2n |   head -1  > ../../plots/$(hostname)'_'$n/Pgi/$matrix/raptor_TAP.txt
+
+
+rm -f $tempFilename
+for j in  `seq 1 $nloops`; do
+    run parallelSpmv $matrix $n | grep by >>  $tempFilename
+done
+cat $tempFilename  | grep process | awk 'BEGIN{}   { printf("%f  %f\n", $7,$10)}  END{}' |  sort  -k1,1n -k2,2n |   head -1  > ../../plots/$(hostname)'_'$n/Pgi/$matrix/parallelSpmv.txt
+
+rm -f $tempFilename
+for j in  `seq 1 $nloops`; do
+    run parallelSpmvSm $matrix $n | grep by >>  $tempFilename
+done
+cat $tempFilename  | grep process | awk 'BEGIN{}   { printf("%f  %f\n", $7,$10)}  END{}' |  sort  -k1,1n -k2,2n |   head -1  > ../../plots/$(hostname)'_'$n/Pgi/$matrix/parallelSpmvSm.txt
+
+rm $tempFilename
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+cd
+echo "bye ..."
 
